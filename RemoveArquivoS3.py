2cf89e1cdbd9a824d90346b7c3889f1ded85f3ce
Guilherme Oliveira da Rosa <guilhermeor.rs@gmail.com>
2017-10-16 01:17:10 -0200

    Remove Arquivo
    Correção na forma que é realizada a remoção de arquivos

--- RemoveArquivoS3.py
+++ RemoveArquivoS3.py
@@ -1,4 +1,16 @@
 import boto3
 
 s3 = boto3.resource('s3')
-s3.Bucket('python-codes').delete_object(Key='novoArquivo.txt')
+s3.Bucket('python-codes').delete_objects
+(
+    bucket.delete_objects
+    (
+    Delete=
+        {
+        'Objects': [
+            {
+                'Key': 'novoArquivo.txt'
+            }]
+        }
+    )
+)
